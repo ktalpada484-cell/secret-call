b9f934568c8d0196c4a86e3bd44cd3a8489da56f
ktalpada484-cell <ktalpada484@gmail.com>
2026-07-28 18:36:54 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files from "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// WebRTC Signaling Handler
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+
+    socket.on('join-room', (roomId) => {
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected', socket.id);
+    });
+
+    socket.on('offer', (data) => {
+        socket.to(data.room).emit('offer', data.offer);
+    });
+
+    socket.on('answer', (data) => {
+        socket.to(data.room).emit('answer', data.answer);
+    });
+
+    socket.on('ice-candidate', (data) => {
+        socket.to(data.room).emit('ice-candidate', data.candidate);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
